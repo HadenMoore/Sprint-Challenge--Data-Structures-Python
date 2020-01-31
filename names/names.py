008c588dcc48500bea996f1cf557c4e9af1aa45e
Haden Moore <hadens.moore@gmail.com>
2020-01-31 10:43:37 -0600

    Minor Addition

--- names/names.py
+++ names/names.py
@@ -19,6 +19,28 @@ for name_1 in names_1:
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+""" 
+
+# Initialize tree with first value of names_1
+tree = BinarySearchTree(names_1[0])
+
+# for each name in names_1
+for name in names_1[1:]:
+    # Insert name into tree
+    tree.insert(name)
+
+# Initialize a dupes list
+duplicates = []
+
+# for each name in names_2
+for name in names_2:
+    # check if tree contains name
+    # If it does
+    if tree.contains(name):
+        # Add it to dupes list
+        duplicates.append(name)
+
+
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
