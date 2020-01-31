4960d3d9976ae3336a35dc124cacbc93ac49afaf
Haden Moore <hadens.moore@gmail.com>
2020-01-31 10:47:26 -0600

    Adding Starter Code RunTime as Comment in names.py

--- names/names.py
+++ names/names.py
@@ -20,6 +20,7 @@ for name_1 in names_1:
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
+    # Starter Code Runtime: 10.759980916976929 seconds
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
