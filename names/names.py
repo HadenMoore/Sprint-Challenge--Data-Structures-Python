610d5397b3da7f1bafaf90b58e50b235791bf40b
Haden Moore <hadens.moore@gmail.com>
2020-01-31 10:46:36 -0600

    Runtime before Optimization:
    Runtime: 10.759980916976929 seconds

--- names/names.py
+++ names/names.py
@@ -1,5 +1,6 @@
 import time
 from bsearchtree import BinarySearchTree
+
 start_time = time.time()
 
 f = open('names_1.txt', 'r')
@@ -19,28 +20,6 @@ for name_1 in names_1:
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-""" 
-
-# Initialize tree with first value of names_1
-tree = BinarySearchTree(names_1[0])
-
-# for each name in names_1
-for name in names_1[1:]:
-    # Insert name into tree
-    tree.insert(name)
-
-# Initialize a dupes list
-duplicates = []
-
-# for each name in names_2
-for name in names_2:
-    # check if tree contains name
-    # If it does
-    if tree.contains(name):
-        # Add it to dupes list
-        duplicates.append(name)
-
-
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
