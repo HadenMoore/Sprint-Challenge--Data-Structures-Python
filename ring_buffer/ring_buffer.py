d6418bebd2ad43849c53bff61064e9d46c52efee
Haden Moore <hadens.moore@gmail.com>
2020-01-31 12:48:03 -0600

    Update ring_buffer.py

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,27 +6,40 @@ class RingBuffer:
         self.capacity = capacity
         self.current = None
         self.storage = DoublyLinkedList()
-
+        self.track = 0
     def append(self, item):
-        pass
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        list_buffer_contents = []
 
-        return list_buffer_contents
+        item = self.storage.head
+        if self.storage.length < 1:
+            return list_buffer_contents
+        else: 
+            while item.next: 
+                list_buffer_contents.append(item.value)
+                item = item.next
+            list_buffer_contents.append(item.value)
+            return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
 
 
 class ArrayRingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity 
+        self.storage = [0]*capacityself.track = 0 
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity: 
+            self.storage.append(item)
+            self.track += 1 
+        else: 
+            self.storage[self.track % self.capacity] = itemself.track += 1
 
     def get(self):
-        pass
+        return [i for i in self.storage if i]
