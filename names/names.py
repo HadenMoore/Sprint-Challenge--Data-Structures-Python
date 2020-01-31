c0dd80d98dce7f308ff14844067e781216d4040d
Haden Moore <hadens.moore@gmail.com>
2020-01-31 11:12:34 -0600

    Optimized Code Test: 0.11665010452270508 seconds
    0.11665010452270508 seconds

--- names/names.py
+++ names/names.py
@@ -12,37 +12,25 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
-
-# Initialize tree with first value of names_1
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 bst = BinarySearchTree(names_1[0])
-
-# for each name in names_1
 for name in names_1[1:]:
-    # Insert name into tree
     bst.insert(name)
 
-# Initialize a dupes list
-duplicates = []
-
-# for each name in names_2
 for name in names_2:
-    # check if tree contains name
-    # If it does
     if bst.contains(name):
-        # Add it to dupes list
         duplicates.append(name)
-
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
+
     # Starter Code Runtime: 10.759980916976929 seconds
-    # Optimized Code Runtime: 6.327238321304321 seconds
+    # Optimized Code Runtime: 0.11665010452270508 seconds
 
 
 
